64736aebce1adb7f5e155d0d0c720b704cec1a92
ashaginyan <anaidashaginyan@yandex.ru>
2019-12-11 17:27:10 +0300

    4.13-4.14

---
+++ src/4_13.js
@@ -0,0 +1,16 @@
+var A = Object.create(Math)
+var B = Object.create(A)
+var C = Object.create(B)
+var D = Object.create(C)
+
+Object.getPrototypeOf(A).f = function(x) {
+  return 2*x + 1
+}
+
+var t = 2
+document.write("Function f("+t+")=" + Math.f(t)+"<br>")
+t = 3
+document.write("Function f("+t+")=" + D.f(t)+"<br>")
+var obj = Object.getPrototypeOf(Object.getPrototypeOf(D))
+
+document.write("obj == B -> " + (obj==B)+"<br")
---
+++ src/4_14.js
@@ -0,0 +1,16 @@
+var A = {}
+var B = Object.create(null)
+var C = Object.create(B)
+B.number = 100
+Object.prototype.name = "Obj A"
+
+show(A)
+show(B)
+show(C)
+
+function show(obj) {
+  for (var s in obj) {
+    document.write(s+"->"+obj[s])
+  }
+  document.write("<br>")
+}
